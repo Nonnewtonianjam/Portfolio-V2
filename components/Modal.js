2ee00b0c18cfebd8859e1e79d7a084bc108b347b
Johnathan Manney <johnathan.a.manney@gmail.com>
2022-07-03 11:16:39 -0400

    Change height of modal for large screens

--- components/Modal.js
+++ components/Modal.js
@@ -12,7 +12,7 @@ const Modal = ({visible, children, onClose}) => {
         onClick={handleOnClose} 
         className='z-30 fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
         
-        <div className={`relative flex h-4/5 lg:h-1/2 md:w-2/3 lg:w-1/2 items-center bg-white dark:bg-black p-2 rounded  overflow-auto ${visible === true ? 'modal active' : 'modal'}`}>
+        <div className={`relative flex h-4/5 lg:h-3/5 md:w-2/3 lg:w-1/2 items-center bg-white dark:bg-black p-2 rounded  overflow-auto ${visible === true ? 'modal active' : 'modal'}`}>
             {children}
             <div className='absolute bottom-4 right-4 button'>
                 <button className='' onClick={onClose}>Close</button>
